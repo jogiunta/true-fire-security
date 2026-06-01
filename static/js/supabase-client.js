3934c26601a97629fa797b51a6ae2f40ec960bd1
jogiunta <jogiunta@iu.edu>
2026-06-01 12:21:14 -0500

    db connection

--- static/js/supabase-client.js
+++ static/js/supabase-client.js
@@ -2,13 +2,3 @@ const SUPABASE_URL = 'https://pbwzpcgjrmvbhpztvcwh.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_blSRkABHYhif93qa7JYfwg_qPVeIJhs'
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-window.addEventListener('load', function() {
-  supabaseClient
-    .from('customers')
-    .select('id')
-    .limit(1)
-    .then(({ data, error }) => {
-      if (error) console.error('Supabase error:', error.message)
-      else console.log('Supabase connected:', data)
-    })
-})
